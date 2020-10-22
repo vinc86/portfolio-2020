8d78be8dbfdce4ecc8cb5f771dc9430890790c83
vinc86 <vincenzomancuso86@gmail.com>
2020-10-23 00:21:25 +0200

    Updates

---
+++ precache-manifest.74bd9a014fe3a2edb9c9b8a8140ac072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23083249772104f5e6c7fbd80de7d351",
+    "url": "/portfolio-2020/index.html"
+  },
+  {
+    "revision": "7de794e84d943b2f18d6",
+    "url": "/portfolio-2020/static/css/main.7184eba0.chunk.css"
+  },
+  {
+    "revision": "47b559cf1c28c58de1d8",
+    "url": "/portfolio-2020/static/js/2.1f8366c6.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/portfolio-2020/static/js/2.1f8366c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de794e84d943b2f18d6",
+    "url": "/portfolio-2020/static/js/main.68bb8fa0.chunk.js"
+  },
+  {
+    "revision": "bbf3efb9002603ffad2a",
+    "url": "/portfolio-2020/static/js/runtime-main.106eb575.js"
+  },
+  {
+    "revision": "65d2822f57d56ab373626fa7f6775c18",
+    "url": "/portfolio-2020/static/media/IMG-20200417-WA0081.65d2822f.jpg"
+  }
+]);
